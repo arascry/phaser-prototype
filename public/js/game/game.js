99e22d0e32893696ecda2a77e472ca18054fbb37
Minkaron <aaron.tucaz@gmail.com>
2020-10-21 16:22:43 -0700

    Experimenting with physics and tilemaps

--- public/js/game/game.js
+++ public/js/game/game.js
@@ -1,8 +1,9 @@
 var config = {
     type: Phaser.AUTO,
-    antialias: false,
-    width: 800,
-    height: 600,
+    pixelArt: true,
+    width: 1000,
+    height: 1000,
+    zoom: 2,
     autoCenter: Phaser.Scale.CENTER_BOTH,
     physics: {
         default: 'arcade',
@@ -19,13 +20,35 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload() {
+    this.load.image('tiles', 'static/img/dungeonWalls.png');
+    this.load.tilemapTiledJSON('map', 'static/img/map.json');
     this.load.spritesheet('megaset', 'static/img/dungeonTileSet.png', { frameWidth: 16, frameHeight: 16 });
 }
 
 function create() {
-    const orcLeader = this.physics.add.image(300, 300, 'megaset', '164').setDisplaySize(100, 100);
-    const orcBrown = this.physics.add.image(400, 300, 'megaset', '163').setDisplaySize(100, 100);
-    const orcGreen = this.physics.add.image(500, 300, 'megaset', '162').setDisplaySize(100, 100);
+
+
+    let map = this.add.tilemap('map');
+    let tiles = map.addTilesetImage('dungeonWalls', 'tiles');
+    let floors = map.createStaticLayer('floors', tiles, 0, 0);
+    let walls = map.createStaticLayer('walls', tiles, 0, 0);
+
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+    walls.setCollisionByProperty({ collides: true });
+
+    const orcLeader = this.physics.add.image(350, 250, 'megaset', '164');
+    const orcBrown = this.physics.add.image(325, 250, 'megaset', '163');
+    const orcGreen = this.physics.add.image(300, 250, 'megaset', '162');
+
+    this.physics.add.collider(orcLeader, orcBrown);
+    this.physics.add.collider(orcLeader, orcGreen);
+    this.physics.add.collider(orcBrown, orcGreen);
+
+    this.physics.add.collider(orcLeader, walls);
+    this.physics.add.collider(orcBrown, walls);
+    this.physics.add.collider(orcGreen, walls);
+
 
     orcLeader.setVelocity(100, 200);
     orcLeader.setBounce(1, 1);
